b42f66df871dfff2dc5c52d88e27cf64030959cb
David Milanese <david.milanese@tenantoptions.com.au>
2019-10-09 09:33:18 +1100

    add test

--- test/unblock_me_solver_test.exs
+++ test/unblock_me_solver_test.exs
@@ -2,7 +2,13 @@ defmodule UnblockMeSolverTest do
   use ExUnit.Case
   doctest UnblockMeSolver
 
-  test "greets the world" do
-    assert UnblockMeSolver.hello() == :world
+  test "when there is 1 block in the way of solving the problem" do
+    assert UnblockMeSolver.problem_chain([
+        ['B', 'B', nil, nil, nil],
+        ['C', 'C', nil, nil, 'F'],
+        ['A', 'A', nil, nil, 'F'],
+        ['D', 'D', nil, nil, nil],
+        ['E', 'E', nil, nil, nil],
+      ]) == ['F']
   end
 end
