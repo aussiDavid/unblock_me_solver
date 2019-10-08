c738bd30706e45a10119d543ed0eb5f999fc6987
David Milanese <david.milanese@tenantoptions.com.au>
2019-10-08 15:41:24 +1100

    Initial commit

---
+++ lib/unblock_me_solver.ex
@@ -0,0 +1,18 @@
+defmodule UnblockMeSolver do
+  @moduledoc """
+  Documentation for UnblockMeSolver.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UnblockMeSolver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/unblock_me_solver_test.exs
@@ -0,0 +1,8 @@
+defmodule UnblockMeSolverTest do
+  use ExUnit.Case
+  doctest UnblockMeSolver
+
+  test "greets the world" do
+    assert UnblockMeSolver.hello() == :world
+  end
+end
