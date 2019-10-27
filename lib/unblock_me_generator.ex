e3b2f02a549bfa0e39a12f00b9c93ab63e28f7a5
David Milanese <milanese.david@gmail.com>
2019-10-27 17:50:49 +1100

    layer generator module

--- lib/unblock_me_generator.ex
+++ lib/unblock_me_generator.ex
@@ -1,19 +1,5 @@
 defmodule UnblockMeGenerator do
-  @moduledoc """
-  UnblockMeGenerator generates UnblockMe problems in various configurations
-
-  The function names provide a clear description of the difficulty of the problems
-
-  example, a trivial problem:
-      iex> UnblockMeGenerator.trivial()
-      [
-        [nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil],
-        ['A', 'A', nil, nil, nil],
-        [nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil]
-      ]
-  """
+  @moduledoc false
 
   @doc """
   Generates a 5 by 5 problem with 1 block.
