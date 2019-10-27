1220d95b76fa9a4c12bccee652c06b6f94611728
David Milanese <milanese.david@gmail.com>
2019-10-27 19:35:23 +1100

    added trvial test

--- test/unblock_me_solver_test.exs
+++ test/unblock_me_solver_test.exs
@@ -1,4 +1,10 @@
 defmodule UnblockMeSolverTest do
   use ExUnit.Case
   doctest UnblockMeSolver
+
+  describe "solve/1" do
+    test "when the problem is already solved" do
+      assert UnblockMeSolver.solve([['A', 'A']]) == []
+    end
+  end
 end
