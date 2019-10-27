864b1f72221a24b3257c983855a3f302b34a70bd
David Milanese <milanese.david@gmail.com>
2019-10-27 17:51:19 +1100

    move tests

--- lib/unblock_me_solver.ex
+++ lib/unblock_me_solver.ex
@@ -1,74 +1,65 @@
 defmodule UnblockMeSolver do
   @moduledoc """
-  Documentation for UnblockMeSolver.
+  UnblockMeSolver is a solver for the UnblockMe mobile puzzle game. 
+
+  This module to generate and solve UnblockMe problems.
+  UnblockMeSolver generates and solves UnblockMe problems in various configurations
+
+  For example, to generate a trivial problem:
+
+      iex> UnblockMeSolver.generate(:trivial)
+      [
+        [nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil],
+        ['A', 'A', nil, nil, nil],
+        [nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil]
+      ]
+
+  Then solving the problem:
+
+      iex> UnblockMeSolver.generate(:trivial) |> UnblockMeSolver.solve()
+      [{'A', :right, 3}]
   """
 
   @doc """
-  Generates a 5 by 5 problem of blocks.
+  Generates a 5 by 5 problem with 1 block.
+
+  Idealy used as a test case, the solution block is unobstructed
 
   ## Examples
 
+      iex> UnblockMeSolver.generate(:trivial)
       [
-        ['B', 'B', nil, nil, nil],
-        ['C', 'C', nil, nil, nil],
+        [nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil],
         ['A', 'A', nil, nil, nil],
-        ['D', 'D', nil, nil, nil],
-        ['E', 'E', nil, nil, nil]
+        [nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil]
       ]
 
   """
-  def generate_problem do
-    [
-      ['B', 'B', nil, 'G', 'G'],
-      ['C', 'C', nil, nil, 'F'],
-      ['A', 'A', nil, nil, 'F'],
-      ['D', 'D', nil, nil, nil],
-      ['E', 'E', nil, nil, nil]
-    ]
+  def generate(difficulty \\ :trivial) do
+    case difficulty do
+      :trivial -> UnblockMeGenerator.trivial
+      _ -> raise "Difficulty level not recognised"
+    end
   end
 
   @doc """
-  Determines if the problem is solvable
-  A problem is solvable when there are empty spaces to the right of the solution block ['A', 'A']
+  Solves an UnblockMe problem generated from `UnblockMeSolver.generate/1` and returns a list of moves to solve the problem
 
   ## Examples
 
-      iex> UnblockMeSolver.solvable?([
-      ...>  ['B', 'B', nil, nil, nil],
-      ...>  ['C', 'C', nil, nil, nil],
-      ...>  ['A', 'A', nil, nil, nil],
-      ...>  ['D', 'D', nil, nil, nil],
-      ...>  ['E', 'E', nil, nil, nil],
-      ...>])
-      true
-  """
-  def solvable? problem do
-    problem
-      |> UnblockMeSolver.extract_solution_row
-      |> Enum.drop_while(fn x -> x != 'A' end)
-      |> Enum.filter(fn x -> x != 'A' end)
-      |> Enum.all?(fn x -> x == nil end)
-  end
-
-  @doc """
-  Computes the moves required to solve the problem
-
-  ## Examples
-
-      iex> UnblockMeSolver.solve([
-      ...>  ['B', 'B', nil, nil, nil],
-      ...>  ['C', 'C', nil, nil, nil],
-      ...>  ['A', 'A', nil, nil, nil],
-      ...>  ['D', 'D', nil, nil, nil],
-      ...>  ['E', 'E', nil, nil, nil],
-      ...>])
+      iex> UnblockMeSolver.generate(:trivial) |> UnblockMeSolver.solve
       [{'A', :right, 3}]
 
   """
-  def solve problem do
-    if UnblockMeSolver.solvable?(problem) do
-      moves = problem
-        |> UnblockMeSolver.extract_solution_row
+  def solve(problem) do
+    if UnblockMeSolver.Move.solvable?(problem) do
+      moves =
+        problem
+        |> UnblockMeSolver.Move.extract_solution_row()
         |> Enum.drop_while(fn x -> x != 'A' end)
         |> Enum.filter(fn x -> x != 'A' end)
         |> Enum.count(fn x -> x == nil end)
@@ -78,70 +69,5 @@ defmodule UnblockMeSolver do
       # UnblockMeSolver.solve problem
       []
     end
-  end
-
-  @doc """
-  Determines the problem chain of blocks stopping the 'A' block from moving to the exit
-
-  ## Examples
-
-      iex> UnblockMeSolver.problem_chain([
-      ...>  ['B', 'B', nil, nil, nil],
-      ...>  ['C', 'C', nil, nil, 'F'],
-      ...>  ['A', 'A', nil, nil, 'F'],
-      ...>  ['D', 'D', nil, nil, nil],
-      ...>  ['E', 'E', nil, nil, nil],
-      ...>])
-      ['F']
-
-  """
-  def problem_chain problem, chain \\ [], iterations \\ 10 do
-    cond do
-      UnblockMeSolver.solvable?(problem) ->
-        chain
-      iterations < 1 ->
-        raise "the solution took too long"
-      true ->
-        problem
-          |> UnblockMeSolver.extract_solution_row
-          |> Enum.filter(fn x -> x != 'A' end)
-          |> Enum.filter(fn x -> x != nil end)
-          |> Enum.uniq
-    end
-  end
-
-  @doc """
-  Extracts the middle row from a problem
-
-  ## Examples
-
-      iex> UnblockMeSolver.extract_solution_row([
-      ...>  ['B', 'B', nil, nil, nil],
-      ...>  ['C', 'C', nil, nil, nil],
-      ...>  ['A', 'A', nil, nil, nil],
-      ...>  ['D', 'D', nil, nil, nil],
-      ...>  ['E', 'E', nil, nil, nil],
-      ...>])
-      ['A', 'A', nil, nil, nil]
-
-  """
-  def extract_solution_row problem do
-    index = length(problem) / 2 |> floor
-    case Enum.fetch problem, index do
-      {:ok, row} -> row
-      _ -> []
-    end
-  end
-
-  def rotate_ccw problem do
-    problem
-      |> Enum.map(&Enum.reverse/1)
-      |> Transpose.transpose
-  end
-
-  def rotate_cw problem do
-    problem
-      |> Transpose.transpose
-      |> Enum.map(&Enum.reverse/1)
   end
 end
