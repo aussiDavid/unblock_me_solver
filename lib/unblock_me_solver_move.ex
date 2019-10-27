669e9823edd0e066026a22af1224bb47ae9c0bd0
David Milanese <milanese.david@gmail.com>
2019-10-27 17:51:12 +1100

    move the implementation logic for solve/1 to a new module

---
+++ lib/unblock_me_solver_move.ex
@@ -0,0 +1,96 @@
+defmodule UnblockMeSolver.Move do
+  alias UnblockMeSolver.Move
+
+  @moduledoc false
+  
+  @doc """
+  Determines if the problem is solvable
+  A problem is solvable when there are empty spaces to the right of the solution block ['A', 'A']
+
+  ## Examples
+
+      iex> Move.solvable?([
+      ...>  ['B', 'B', nil, nil, nil],
+      ...>  ['C', 'C', nil, nil, nil],
+      ...>  ['A', 'A', nil, nil, nil],
+      ...>  ['D', 'D', nil, nil, nil],
+      ...>  ['E', 'E', nil, nil, nil],
+      ...>])
+      true
+  """
+  def solvable?(problem) do
+    problem
+    |> Move.extract_solution_row()
+    |> Enum.drop_while(fn x -> x != 'A' end)
+    |> Enum.filter(fn x -> x != 'A' end)
+    |> Enum.all?(fn x -> x == nil end)
+  end
+
+  @doc """
+  Determines the problem chain of blocks stopping the 'A' block from moving to the exit
+
+  ## Examples
+
+      iex> Move.problem_chain([
+      ...>  ['B', 'B', nil, nil, nil],
+      ...>  ['C', 'C', nil, nil, 'F'],
+      ...>  ['A', 'A', nil, nil, 'F'],
+      ...>  ['D', 'D', nil, nil, nil],
+      ...>  ['E', 'E', nil, nil, nil],
+      ...>])
+      ['F']
+
+  """
+  def problem_chain(problem, chain \\ [], iterations \\ 10) do
+    cond do
+      Move.solvable?(problem) ->
+        chain
+
+      iterations < 1 ->
+        raise "the solution took too long"
+
+      true ->
+        problem
+        |> Move.extract_solution_row()
+        |> Enum.filter(fn x -> x != 'A' end)
+        |> Enum.filter(fn x -> x != nil end)
+        |> Enum.uniq()
+    end
+  end
+
+  @doc """
+  Extracts the middle row from a problem
+
+  ## Examples
+
+      iex> Move.extract_solution_row([
+      ...>  ['B', 'B', nil, nil, nil],
+      ...>  ['C', 'C', nil, nil, nil],
+      ...>  ['A', 'A', nil, nil, nil],
+      ...>  ['D', 'D', nil, nil, nil],
+      ...>  ['E', 'E', nil, nil, nil],
+      ...>])
+      ['A', 'A', nil, nil, nil]
+
+  """
+  def extract_solution_row(problem) do
+    index = (length(problem) / 2) |> floor
+
+    case Enum.fetch(problem, index) do
+      {:ok, row} -> row
+      _ -> []
+    end
+  end
+
+  def rotate_ccw(problem) do
+    problem
+    |> Enum.map(&Enum.reverse/1)
+    |> Transpose.transpose()
+  end
+
+  def rotate_cw(problem) do
+    problem
+    |> Transpose.transpose()
+    |> Enum.map(&Enum.reverse/1)
+  end
+end
---
+++ test/unblock_me_generator_move.exs
@@ -0,0 +1,108 @@
+defmodule UnblockMeSolver.Move.MoveTest do
+  use ExUnit.Case
+  doctest UnblockMeSolver.Move.Move
+
+  describe "problem_chain/1" do
+    test "when there are no blocks in the way of solving the problem" do
+      assert UnblockMeSolver.Move.problem_chain([
+               ['B', 'B', nil, nil, nil],
+               ['C', 'C', nil, nil, nil],
+               ['A', 'A', nil, nil, nil],
+               ['D', 'D', nil, nil, nil],
+               ['E', 'E', nil, nil, nil]
+             ]) == []
+    end
+
+    test "when there is 1 block in the way and a block that can be ignored" do
+      assert UnblockMeSolver.Move.problem_chain([
+               [nil, nil, nil, 'G', 'G'],
+               [nil, nil, nil, nil, 'F'],
+               ['A', 'A', nil, nil, 'F'],
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil]
+             ]) == ['F']
+    end
+
+    # test "when there are 2 blocks in the way and a block that can be ignored" do
+    #   assert UnblockMeSolver.Move.problem_chain([
+    #       [nil, nil, nil, 'G', 'G'],
+    #       [nil, nil, nil, nil, 'F'],
+    #       ['A', 'A', nil, nil, 'F'],
+    #       [nil, nil, nil, 'H', 'H'],
+    #       [nil, nil, nil, nil, nil],
+    #     ]) == ['F', 'G']
+    # end
+  end
+
+  describe "solvable?/1" do
+    test "when a block is in the way on the last column" do
+      assert UnblockMeSolver.Move.solvable?([
+               ['B', 'B', nil, nil, nil],
+               ['C', 'C', nil, nil, 'F'],
+               ['A', 'A', nil, nil, 'F'],
+               ['D', 'D', nil, nil, nil],
+               ['E', 'E', nil, nil, nil]
+             ]) == false
+    end
+
+    test "when the problem is solvable with a block in the last column" do
+      assert UnblockMeSolver.Move.solvable?([
+               [nil, nil, nil, nil, 'F'],
+               [nil, nil, nil, nil, 'F'],
+               ['A', 'A', nil, nil, nil],
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil]
+             ]) == true
+    end
+
+    test "when the problem is solvable with a block in the last column at the bottom" do
+      assert UnblockMeSolver.Move.solvable?([
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil],
+               ['A', 'A', nil, nil, nil],
+               [nil, nil, nil, nil, 'F'],
+               [nil, nil, nil, nil, 'F']
+             ]) == true
+    end
+
+    test "when the problem is solvable with the solution block starting at the 2nd column" do
+      assert UnblockMeSolver.Move.solvable?([
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil],
+               [nil, 'A', 'A', nil, nil],
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil]
+             ]) == true
+    end
+
+    test "when the problem is solvable with the solution block starting at the 3rd column" do
+      assert UnblockMeSolver.Move.solvable?([
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil],
+               [nil, nil, 'A', 'A', nil],
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil]
+             ]) == true
+    end
+
+    test "when the problem is solvable with the solution as at the last column" do
+      assert UnblockMeSolver.Move.solvable?([
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, 'A', 'A'],
+               [nil, nil, nil, nil, nil],
+               [nil, nil, nil, nil, nil]
+             ]) == true
+    end
+
+    test "when the problem is solvable with a block behind the solution block" do
+      assert UnblockMeSolver.Move.solvable?([
+          [nil, nil, nil, nil, nil],
+          ['B', nil, nil, nil, nil],
+          ['B', 'A', 'A', nil, nil],
+          [nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil]
+        ]) == true
+    end
+  end
+end
