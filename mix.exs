04a57c518078427d2e2a100ff2d085b985091fd4
David Milanese <david.milanese@tenantoptions.com.au>
2019-10-27 16:45:03 +1100

    update dependancies

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule UnblockMeSolver.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "UnblockMeSolver",
+      source_url: "https://github.com/aussiDavid/unblock_me_solver",
+      homepage_url: "https://github.com/aussiDavid/unblock_me_solver",
+      docs: [
+        main: "UnblockMeSolver",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -23,7 +32,8 @@ defmodule UnblockMeSolver.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 end
